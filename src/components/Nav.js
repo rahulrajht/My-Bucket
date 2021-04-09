d06b8de5fc79d47b0ef0b3d6621d36cafce7835e
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2021-04-09 12:50:06 +0530

    Changes in navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,4 @@
-import cart from "../images/cart.png";
-import wish from "../images/wish.png";
 import "../styles/nav.css";
-import { useRef } from "react";
 import { useUrl } from "../context/useUrl";
 import { useCart } from "../context/cartContext";
 import { Link } from "react-router-dom";
@@ -9,15 +6,13 @@ export default function Nav() {
   const { wishList, cartItems } = useCart();
 
   const { setUrl } = useUrl();
-  const cartRef = useRef();
 
   return (
     <nav className="navbars">
       <div className="side-bar">
         <ul>
           <li onClick={() => setUrl("https://fakestoreapi.com/products")}>
-            {" "}
-            All{" "}
+            All
           </li>
           <li
             onClick={() =>
@@ -26,102 +21,43 @@ export default function Nav() {
               )
             }
           >
-            {" "}
-            Women's{" "}
+            Women's
           </li>
           <li
             onClick={() =>
               setUrl("https://fakestoreapi.com/products/category/men clothing")
             }
           >
-            {" "}
-            Men's{" "}
+            Men's
           </li>
           <li
             onClick={() =>
               setUrl("https://fakestoreapi.com/products/category/electronics")
             }
           >
-            {" "}
-            Electronics{" "}
+            Electronics
           </li>
           <li
             onClick={() =>
               setUrl("https://fakestoreapi.com/products/category/jewelery")
             }
           >
-            {" "}
-            Jewelery{" "}
+            Jewelery
           </li>
         </ul>
       </div>
       <ul>
-        <Link to="/cart">
-          <div
-            style={{
-              position: "relative"
-            }}
-          >
-            <img
-              style={{
-                width: "40px",
-                position: "relative"
-              }}
-              ref={cartRef}
-              src={cart}
-              alt=" "
-            />
-            <span
-              style={{
-                position: "absolute",
-                width: "20px",
-                top: "-5px",
-                right: "-2px",
-                backgroundColor: "white",
-                borderRadius: "50%",
-                fontSize: "1rem",
-                padding: "0.25rem"
-              }}
-            >
-              {" "}
-              {cartItems.length}{" "}
-            </span>
-          </div>
-        </Link>
-        <Link to="/wishlist">
-          <div
-            style={{
-              position: "relative",
-              marginLeft: "5px"
-            }}
-          >
-            <img
-              style={{
-                width: "40px",
-                position: "relative"
-              }}
-              src={wish}
-              alt=" "
-            />
-            <span
-              style={{
-                position: "absolute",
-                width: "20px",
-                top: "-5px",
-                right: "-2px",
-                backgroundColor: "white",
-                borderRadius: "50%",
-                fontSize: "1rem",
-                padding: "0.25rem"
-              }}
-            >
-              {" "}
-              {wishList ? wishList.length : 0}{" "}
-            </span>
-          </div>
-        </Link>
-        <div>
-          <button> Toggle </button>
+        <div style={{ position: "relative", marginLeft: "5px" }}>
+          <Link to="/cart" className="cart">
+            {" "}
+          </Link>
+          <span className="span-img">{cartItems.length}</span>
+        </div>
+        <div style={{ position: "relative", marginLeft: "5px" }}>
+          <Link to="/wishlist" className="wishlist">
+            {" "}
+          </Link>
+          <span className="span-img">{wishList.length}</span>
         </div>
       </ul>
     </nav>
