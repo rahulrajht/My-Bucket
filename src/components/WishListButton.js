2fa800bc58b8af4fff30e0ddca0a8b61801766db
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2021-04-16 17:18:17 +0530

    fixed wishlist button

--- src/components/WishListButton.js
+++ src/components/WishListButton.js
@@ -1,16 +1,27 @@
 import { checkItemInCart } from "../utils/checkItemInCart";
 import { useCart } from "../context/cartContext";
 import wish from "../images/wish.png";
+import redwish from "../images/redwish.png";
 import "../styles/wishList.css";
+import { useState } from "react";
 
 export const WishlistButton = (item) => {
   const { wishList, dispatchData } = useCart();
+  const [isTrue, setTrue] = useState(false);
   const handleClick = () => {
+    setTrue(!isTrue);
     if (checkItemInCart(wishList, item.item.id)) {
       dispatchData({ type: "removeWishlistItem", id: item.item.id });
     } else {
       dispatchData({ type: "addWishlistItem", items: item.item });
     }
   };
-  return <img onClick={handleClick} className="wishImage" src={wish} alt=" " />;
+  return (
+    <img
+      onClick={handleClick}
+      className="wishImage"
+      src={isTrue ? redwish : wish}
+      alt=" "
+    />
+  );
 };
