fca4a2fd13651deff328b12a051cf5c4559d6f74
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2021-04-29 11:13:56 +0530

    Removed unused files

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { CartProvider } from "./context/cartContext";
-import { ScreenProvider } from "./context/changeScreen";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <CartProvider>
-      <ScreenProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ScreenProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </CartProvider>
   </StrictMode>,
   rootElement
