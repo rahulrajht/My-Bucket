063b1a25a52f2b21eab8d722f72ad65e115e487e
Rahul kumar <63516339+rahulrajht@users.noreply.github.com>
2021-04-30 23:09:43 +0530

    Update Rating.js

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -7,14 +7,14 @@ export default function Rating({ rt }) {
     color = "badge--red";
   }
   if (rt >= 2 && rt <= 3) color = "badge--yellow";
-  if (rt >= 3 && rt <= 5) color = "badge-green";
+  if (rt >= 3 && rt <= 5) color = "badge--green";
   return (
     <>
       <div className="card-badges">
         <div className="rating">
           <span className={`badge ${color}`}>
             {rt}
-            <img className="rating-img" width="15px" src={star} alt="" />
+            <img className="rating-img" width="15px" src={star} alt="rating" />
           </span>
         </div>
       </div>
